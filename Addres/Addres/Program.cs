e449a60f3f39a6c50c26929980682d6c462117d5
NAHIN <1521926@iub.edu.bd>
2019-09-10 14:50:19 +0600

    filr_upload

---
+++ Addres/Addres/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Name = "";
+            Console.WriteLine("Enter You Name Please");
+            Name = Console.ReadLine();
+            String Age = "";
+            Console.WriteLine("Enter You Age Please");
+            Age = Console.ReadLine();
+            String Address = "";
+            Console.WriteLine("Enter You Address Please");
+            Address = Console.ReadLine();
+            String Contact = "";
+            Console.WriteLine("Enter You Contact Please");
+            Contact = Console.ReadLine();
+            Console.WriteLine("Profile Details");
+            Console.WriteLine("Name:" +Name);
+            Console.WriteLine("Age:" +Age);
+            Console.WriteLine("Address:" +Address);
+            Console.WriteLine("Contact:" +Contact);
+            Console.ReadKey();
+        }
+    }
+}
